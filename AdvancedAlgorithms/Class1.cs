baf3c783f6c7c63601e34bad28b8e8d1db48af82
Alex Santos <arkanjo.alex@gmail.com>
2021-02-01 20:52:04 -0300

    Inicial commit

---
+++ AdvancedAlgorithms/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdvancedAlgorithms
+{
+    public class Class1
+    {
+    }
+}
---
+++ DataStructureExercise/DataStructure-Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructure_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
